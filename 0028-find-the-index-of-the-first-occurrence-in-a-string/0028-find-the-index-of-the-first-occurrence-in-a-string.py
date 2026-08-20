97fd9ba415c3bf15e2eec142cb61beb3cff1181d
khushi-mavi <khushimavi13@gmail.com>
2026-08-20 19:30:28 +0530

    Time: 1 ms (23.39%), Space: 19.6 MB (8.3%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -7,32 +7,32 @@ class Solution:
         if m > n:
             return -1
         lps = [0]*m
-        self.buildlps(needle, lps)
+        self.buildlps(needle,lps)
         j = 0
-        for i in range(n):
+        for i in range (n):
             while j > 0 and haystack[i] != needle[j]:
                 j = lps[j-1]
             if haystack[i] == needle[j]:
                 j += 1
             if j == m:
-                return i-m + 1
+               return i - m + 1
         return -1
-    def buildlps(self, needle:str, lps:list)-> None:
-            m = len(needle)
-            length = 0
-            i = 1
-            lps[0] = 0
-            while i< m:
-                if needle[i] == needle[length]:
-                    length += 1
-                    lps[i] = length
-                    i += 1
-                else:
-                    if length != 0:
-                        length = lps[length - 1]
-                    else:
-                        lps[i] = 0
-                        i += 1
 
-            
-        
+
+
+    def buildlps(self, needle: str, lps: list)-> None:
+       m = len(needle)
+       length = 0
+       i = 1
+       lps[0] = 0
+       while i < m:
+        if needle[i] == needle[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else: 
+            if length != 0:
+                length = lps[length-1]
+            else:
+                lps[i] = 0
+                i += 1
